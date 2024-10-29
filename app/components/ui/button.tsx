e20357122de80ceb428cd64252f9ec6757ab05c4
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-10-29 00:27:16 -0400

    Resized Landing buttons and Info Cards to match design

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/styles";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-lg font-small transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
 
         permlink: "text-primary underline-offset-4 underline font-semibold",
-        active: "border bg-sgrey-8 border-sgrey-20",
+        active: "border bg-sgrey-8 border-sgrey-15",
       },
       size: {
         default: "h-11 px-6 py-2",
