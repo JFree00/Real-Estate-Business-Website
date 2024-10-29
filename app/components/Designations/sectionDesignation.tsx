b49aefd017e64946f825c4229347564f78ff3164
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-10-29 04:25:47 -0400

    sectionDesignation.tsx can now scale with the main grids rows with its rows prop

--- app/components/Designations/sectionDesignation.tsx
+++ app/components/Designations/sectionDesignation.tsx
@@ -7,9 +7,14 @@ type Props = {
   rows?: number;
 };
 
-export function SectionDesignation({ children }: Props) {
+export function SectionDesignation({ rows = 2, columns = 2, children }: Props) {
   return (
-    <div className={` w-full col-span-10 col-start-2 row-span-6`} style={{}}>
+    <div
+      className={` w-full col-span-10 col-start-2 row-span-6`}
+      style={{
+        gridRow: `span ${rows} / span ${rows}`,
+      }}
+    >
       {children}
     </div>
   );
