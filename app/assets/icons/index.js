a008af734204848b671f5a8c6c2fc849be164125
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-10-30 03:41:17 -0400

    fixed expired imports and pushed tseslint config

--- app/assets/icons/index.js
+++ app/assets/icons/index.js
@@ -1,6 +1,4 @@
-export { default as BedIcon } from "./BedIcon.jsx";
 export { default as LogoLg } from "./LogoLg.jsx";
 export { default as LogoMd } from "./LogoMd.jsx";
 export { default as LogoSm } from "./LogoSm.jsx";
 export { default as Symbol } from "./Symbol.jsx";
-export { default as Waves } from "./Waves.jsx";
