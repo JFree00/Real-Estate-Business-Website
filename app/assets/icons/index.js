8301186b610c40e12b2c723896017a4d4070d21c
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-10-30 03:33:40 -0400

    fixed expired import

--- app/assets/icons/index.js
+++ app/assets/icons/index.js
@@ -1,5 +1,4 @@
 export { default as BedIcon } from "./BedIcon.jsx";
-export { default as CircleText } from "./CircleText.jsx";
 export { default as LogoLg } from "./LogoLg.jsx";
 export { default as LogoMd } from "./LogoMd.jsx";
 export { default as LogoSm } from "./LogoSm.jsx";
