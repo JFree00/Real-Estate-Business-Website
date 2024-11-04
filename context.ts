869925f247c39b913c41032454a57533e62c4c73
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-11-04 08:06:57 -0500

    wrangler testing

--- context.ts
+++ context.ts
@@ -1,10 +1,8 @@
 import { AppLoadContext } from "@remix-run/cloudflare";
 
 // biome-ignore lint/suspicious/noEmptyInterface: Fill this in with your own types for your use-case
-
-export interface Env {
-  deploymentPage: string;
-}
+// eslint-disable-next-line   @typescript-eslint/no-empty-object-type
+export interface Env {}
 
 export function getLoadContext(env: Env): AppLoadContext {
   return { env };
