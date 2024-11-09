acb63df80bdf3ec1cf68b07793e2fd02d491f5b5
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-11-09 00:25:56 -0500

    refactored sectionCards.tsx to a compound for improved customization

--- app/components/cards/sectionCards.tsx
+++ app/components/cards/sectionCards.tsx
@@ -2,48 +2,72 @@
 import * as React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/styles";
 
-export type sectionCardProps = React.HTMLAttributes<HTMLDivElement> & {
+export type sectionCardProps = {
   name: string;
   description: string;
   buttonText?: string;
 };
 
-type Props = {
+type Props = React.HTMLAttributes<HTMLDivElement> & {
   data?: sectionCardProps;
 };
 
-export function SectionCards({ data }: Props) {
+function Content({ data, className }: Props) {
+  data = data as sectionCardProps;
+  return (
+    <CardContent
+      className={cn(
+        "flex justify-start items-center  desktop:px-6 desktop:pb-2 desktop:pt-6",
+        className,
+      )}
+    >
+      {data.buttonText ? (
+        <Button size={"responsive"} variant={"secondary"} className={"text-lg"}>
+          {data.buttonText}
+        </Button>
+      ) : null}
+    </CardContent>
+  );
+}
+
+function Header({ data, className }: Props) {
+  data = data as sectionCardProps;
+  return (
+    <CardHeader
+      className={cn(" desktop:px-6 desktop:pb-2 desktop:pt-6 ", className)}
+    >
+      <CardTitle
+        className={
+          "text-xl lg:text-2xl font-semibold leading-normal min-h-[72px]"
+        }
+      >
+        {data.name}
+      </CardTitle>
+      <p className={"pt-6 lg:text-left max-h-32 text-sgrey-60  min-h-[80px]"}>
+        {data.description}
+      </p>
+    </CardHeader>
+  );
+}
+
+function SectionCards({ data, children }: Props) {
   data = data as sectionCardProps;
   return (
     <div key={data.name} className={"dataCard"}>
-      <Card className={"bg-sgrey-8 dataCardComponent"}>
-        <CardHeader className={" 2xl:px-6 2xl:pb-2 2xl:pt-6 "}>
-          <CardTitle
-            className={
-              "text-xl lg:text-2xl font-semibold leading-normal min-h-[72px]"
-            }
-          >
-            {data.name}
-          </CardTitle>
-          <p
-            className={"pt-6 lg:text-left max-h-32 text-sgrey-60  min-h-[80px]"}
-          >
-            {data.description}
-          </p>
-        </CardHeader>
-        <CardContent className={"flex justify-start items-center"}>
-          {data.buttonText ? (
-            <Button
-              size={"responsive"}
-              variant={"secondary"}
-              className={"text-lg"}
-            >
-              {data.buttonText}
-            </Button>
-          ) : null}
-        </CardContent>
-      </Card>
+      {!children ? (
+        <Card className={"bg-sgrey-8 dataCardComponent"}>
+          <Header data={data} />
+          <Content data={data} />
+        </Card>
+      ) : (
+        <Card className={"bg-sgrey-8 dataCardComponent"}>{children}</Card>
+      )}
     </div>
   );
 }
+SectionCards.Header = Header;
+SectionCards.Content = Content;
+
+export { SectionCards };
