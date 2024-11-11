843c10b8b9484495351e993ef5a1984567c54283
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-11-11 04:03:34 -0500

    added mobile designs

---
+++ KV/steps.ts
@@ -0,0 +1,32 @@
+export type stepsProps = {
+  step: number;
+  name: string;
+  description: string;
+};
+
+export const steps: stepsProps[] = [
+  {
+    step: 1,
+    name: "Discover a World of Possibilities",
+    description:
+      "Your journey begins with exploring our carefully curated property listings. Use our intuitive search tools to filter properties based on your preferences, including location,",
+  },
+  {
+    step: 2,
+    name: "Narrowing Down Your Choices",
+    description:
+      "Once you've found properties that catch your eye, save them to your account or make a shortlist. This allows you to compare and revisit your favorites as you make your decision.",
+  },
+  {
+    step: 3,
+    name: "Making an Offer",
+    description:
+      "Ready to make an offer? Our team of experts will guide you through the process, ensuring you get the best deal possible.",
+  },
+  {
+    step: 4,
+    name: "Personalized Guidance",
+    description:
+      "Have questions about a property or need more information? Our dedicated team of real estate experts is just a call or message away.",
+  },
+];
