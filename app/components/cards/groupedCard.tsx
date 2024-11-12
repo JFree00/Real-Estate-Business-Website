76250c73002b688e21b1f920ff67879c6a99c53e
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-11-12 02:40:47 -0500

    laptop design changes

--- app/components/cards/groupedCard.tsx
+++ app/components/cards/groupedCard.tsx
@@ -13,9 +13,16 @@ export function GroupedCard() {
         "basis-full outline outline-[6px] outline-sgrey-10 rounded-xl border border-sgrey-15"
       }
     >
-      <div className={" mx-auto my-1 w-full flex flex-col"}>
+      <div
+        className={
+          " mx-auto my-1 w-full flex flex-col laptop:flex-row laptop:flex-wrap"
+        }
+      >
         {data.map((item, index) => (
-          <div key={index} className={"basis-1/4 px-6 py-2"}>
+          <div
+            key={index}
+            className={"basis-1/4 laptop:basis-1/2 laptop:shrink-0 px-6 py-2"}
+          >
             <div className={"flex items-center gap-x-2 mb-2"}>
               <div
                 className={
@@ -29,8 +36,18 @@ export function GroupedCard() {
               <p className={" text-lg font-semibold"}>{item.name}</p>
             </div>
 
-            <p className={"text-sm text-sgrey-60"}>{item.description}</p>
-            {index + 2 > data.length ? null : <Separator className={"mt-3"} />}
+            <p
+              className={
+                "text-sm text-sgrey-60 laptop:min-h-16 divide-sgrey-15"
+              }
+            >
+              {item.description}
+            </p>
+            {index + 2 > data.length ? null : (
+              <Separator
+                className={index === 0 || index === 1 ? "mt-3" : "hidden"}
+              />
+            )}
           </div>
         ))}
       </div>
