10f29bce26efaf429325e4c4a48c023a24a25159
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-11-19 00:19:32 -0500

    improved typings and added JSDoc comments

--- KV/filter.ts
+++ KV/filter.ts
@@ -1,17 +1,24 @@
-type filterTypes = "L" | "PT" | "PR" | "PS" | "BY"; //abbreviations stored in the KV to reduce the total length (ex. "build_year: 1997" -> "BY-1997")
-export type abbreviatedFilterKey = `${filterTypes}-${string}`;
-export type nonAbbreviatedFilterKey = `${filterCategories}-${string}`;
+//cloud flares free tier limit is 1k list() calls per day, and 100k read() calls per day (1:100 ratio), and will error if exceeded
+//using these two functions, the only way to provide filter options to the user would be to list() the entire namespace everytime
+//the options are needed, and everytime the user applies filters, which with so many list() calls, can quickly exceed the free tier limits
+//approx. (1 initial list() call for filter options) + (1 list() whenever the filters are changed) equivalent to 200 get() calls as per ratio
+//using this class, no list() calls are needed, and are both replaced with get() calls
+
+//TODO implement multiple filter cursors
+type filterTypes = "L" | "PT" | "PR" | "PS" | "BY"; //abbreviations stored in the cursor to reduce the total length (ex. "build_year: 1997" -> "BY-1997")
 export type filterCategories = //required props in propertyProps
   "property_type" | "location" | "build_year" | "price" | "size";
+export type abbreviatedFilterKey = `${filterTypes}-${string}`;
+export type nonAbbreviatedFilterKey = `${filterCategories}-${string}`;
 export type filteredData = Array<[abbreviatedFilterKey, string[]]>; //parsed cursor KV value
-export type convertedFilter = Map<abbreviatedFilterKey, string[]>; //type clientFilters = [string, string[]];
+export type convertedFilter = Map<abbreviatedFilterKey, string[]>;
 type filterDataParams = {
   [key in filterCategories]: string | number;
 } & {
   name: string;
 };
 
-export class FilterClass {
+export class Filter {
   static readonly keys: { [key in filterCategories]: filterTypes } = {
     property_type: "PT",
     location: "L",
@@ -47,31 +54,32 @@ export class FilterClass {
       [value],
     ];
   }
-  static toCursor(data: filteredData): string {
-    return JSON.stringify(data);
-  }
   static fromCursor(cursor: string): filteredData {
     return JSON.parse(cursor);
   }
-  static mapAllFilters(
-    properties: filterDataParams[],
+  /** Assign all data values to their respective filter keys {@link filterCategories}
+   * @param data Any data extending {@link filterDataParams}
+   * @param filter Only return specified filters
+   */
+  static withAnyFilter(
+    data: filterDataParams[],
     filter?: filterCategories[],
   ): filteredData {
-    const filterValues = new Map() as convertedFilter;
+    const filterValues: convertedFilter = new Map();
     if (filter) {
-      properties.forEach((property) => {
+      data.forEach((property) => {
         filter.forEach((f) => {
           const currentFilter = filterValues.get(
-            FilterClass.abbreviate(f, property[f].toString()),
+            Filter.abbreviate(f, property[f].toString()),
           );
-          filterValues.set(FilterClass.abbreviate(f, property[f].toString()), [
+          filterValues.set(Filter.abbreviate(f, property[f].toString()), [
             ...(currentFilter ? currentFilter : []),
             property.name,
           ]);
         });
       });
     } else {
-      properties.forEach((property) => {
+      data.forEach((property) => {
         Object.entries(this.keys).forEach(([key, value]) => {
           const currentFilter = filterValues.get(
             `${value}-${property[key as filterCategories].toString()}`,
@@ -85,7 +93,8 @@ export class FilterClass {
     }
     return Array.from(filterValues).sort();
   }
-  static filteredResult(
+  /** Return an array of KV names whose data fits every filter*/
+  static withEveryFilter(
     cursor: filteredData,
     filters: Array<abbreviatedFilterKey>,
   ) {
@@ -109,7 +118,8 @@ export class FilterClass {
     });
     return dataIndexes.filter((data) => data) as string[];
   }
-  static toFiltersMapped(
+  /** Return an array of filter categories and their subcategories for popover*/
+  static onlyFilterNames(
     cursor: filteredData,
   ): Array<[filterCategories, string[]]> {
     const filtersArray = new Map<filterCategories, string[]>();
