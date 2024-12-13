813741584d4c79090598bb237012b0de4df2b9b2
JFree00 <jasonjamalfreeman10@yahoo.com>
2024-12-13 00:28:55 -0500

    test function fixes

--- appwrite/functions/test/src/index.ts
+++ appwrite/functions/test/src/index.ts
@@ -1,4 +1,5 @@
-import { Context } from "../../../types";
+import { Context } from "../types.js";
+
 export default async function ({ res }: Context) {
   return res.json({
     message: "Hello World",
